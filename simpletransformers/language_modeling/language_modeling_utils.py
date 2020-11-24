31dd59d1bc5fef04b0d2a91731247a31297b33fc
Ivan Pereira <navi1921@gmail.com>
2020-11-24 14:10:30 -0300

    Update language_modeling_utils.py

--- simpletransformers/language_modeling/language_modeling_utils.py
+++ simpletransformers/language_modeling/language_modeling_utils.py
@@ -55,9 +55,9 @@ def encode_sliding_window(data):
 
 
 class SimpleDataset(Dataset):
-    def __init__(self, tokenizer, args, file_path, mode, block_size=512, special_tokens_count=2, sliding_window=False):
+    def __init__(self, tokenizer, args, folder_path, mode, block_size=512, special_tokens_count=2, sliding_window=False):
         #assert os.path.isfile(file_path)
-        files_paths = glob.glob(file_path)
+        files_paths = glob.glob(folder_path)
         c = 0
         for file_path in files_paths:
             c+=1
